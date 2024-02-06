9fd52871c58cceb87a37810912b25717e145215f
João <joaocdvancea@gmail.com>
2024-02-05 21:41:31 -0300

    barra categorias semipronta

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -18,19 +19,154 @@ class HomePage extends StatelessWidget {
         children: [
           Column(
             children: [
+              const SizedBox(
+                height: 10,
+              ),
               SizedBox(
-                  width: 300,
-                  height: 50,
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                        isDense: true,
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide(
-                          color: Colors.grey,
-                        )),
-                        label: Icon(Icons.search)),
-                  ))
+                width: 300,
+                height: 50,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    isDense: true,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    prefixIcon: Icon(Icons.search),
+                  ),
+                ),
+              ),
             ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Column(
+                children: [
+                  Container(
+                      margin: const EdgeInsets.all(8),
+                      padding: const EdgeInsets.all(8),
+                      width: 50,
+                      height: 50,
+                      decoration: const BoxDecoration(
+                        color: Colors.black,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        MdiIcons.sofaSingleOutline,
+                        color: Colors.white,
+                      )),
+                  Text(
+                    'Móveis',
+                    style: GoogleFonts.roboto(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                      margin: const EdgeInsets.all(8),
+                      padding: const EdgeInsets.all(8),
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        MdiIcons.controllerClassicOutline,
+                        color: Colors.black,
+                      )),
+                  Text(
+                    'Jogos',
+                    style: GoogleFonts.roboto(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                      margin: const EdgeInsets.all(8),
+                      padding: const EdgeInsets.all(8),
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        MdiIcons.headphones,
+                        color: Colors.black,
+                      )),
+                  Text(
+                    'Eletrônicos',
+                    style: GoogleFonts.roboto(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                      margin: const EdgeInsets.all(8),
+                      padding: const EdgeInsets.all(8),
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        MdiIcons.foodAppleOutline,
+                        color: Colors.black,
+                      )),
+                  Text(
+                    'Comida',
+                    style: GoogleFonts.roboto(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                      margin: const EdgeInsets.all(8),
+                      padding: const EdgeInsets.all(8),
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        MdiIcons.shoeSneaker,
+                        color: Colors.black,
+                      )),
+                  Text(
+                    'Calçados',
+                    style: GoogleFonts.roboto(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              /*Container(
+                margin: const EdgeInsets.all(8),
+                padding: const EdgeInsets.all(8),
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(MdiIcons.hanger, color: Colors.black,)
+              ),*/
+            ],
+          ),
+          const SizedBox(
+            height: 24,
           ),
         ],
       ),
